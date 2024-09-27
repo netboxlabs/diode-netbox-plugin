be108dc3e92ec50bc7d5babf87fe5c3fcbaa0afb
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-09-27 12:13:23 +0100

    python fmt
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- netbox_diode_plugin/__init__.py
+++ netbox_diode_plugin/__init__.py
@@ -19,16 +19,12 @@ class NetBoxDiodePluginConfig(PluginConfig):
     default_settings = {
         # Auto-provision users for Diode plugin
         "auto_provision_users": False,
-
         # Default Diode gRPC target for communication with Diode server
         "diode_target": "grpc://localhost:8080/diode",
-
         # User allowed for Diode to NetBox communication
         "diode_to_netbox_username": "diode-to-netbox",
-
         # User allowed for NetBox to Diode communication
         "netbox_to_diode_username": "netbox-to-diode",
-
         # User allowed for data ingestion
         "diode_username": "diode-ingestion",
     }
