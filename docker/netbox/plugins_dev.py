13cb054893c60c9a9512ce838f1e10b49b3bd16a
Luke Tucker <ltucker@netboxlabs.com>
2024-12-12 11:51:23 -0500

    add development setting defaults

---
+++ docker/netbox/plugins_dev.py
@@ -0,0 +1,18 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+PLUGINS = [
+    "netbox_diode_plugin",
+    "netbox_branching",
+]
+
+PLUGINS_CONFIG = {
+    "netbox_diode_plugin": {
+        "auto_provision_users": True,
+        # Diode gRPC target for communication with Diode server
+        "diode_target_override": "grpc://host.docker.internal:8080/diode",
+    }
+}
