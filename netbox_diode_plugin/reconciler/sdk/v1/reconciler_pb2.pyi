eb3709da7e8d0042639b1451142a3932f3be89a2
Luke Tucker <ltucker@netboxlabs.com>
2025-01-21 14:35:38 -0500

    update protobufs

--- netbox_diode_plugin/reconciler/sdk/v1/reconciler_pb2.pyi
+++ netbox_diode_plugin/reconciler/sdk/v1/reconciler_pb2.pyi
@@ -10,42 +10,22 @@ DESCRIPTOR: _descriptor.FileDescriptor
 
 class State(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
-    UNSPECIFIED: _ClassVar[State]
+    STATE_UNSPECIFIED: _ClassVar[State]
     QUEUED: _ClassVar[State]
-    RECONCILED: _ClassVar[State]
+    OPEN: _ClassVar[State]
+    APPLIED: _ClassVar[State]
     FAILED: _ClassVar[State]
     NO_CHANGES: _ClassVar[State]
     IGNORED: _ClassVar[State]
-UNSPECIFIED: State
+    ERRORED: _ClassVar[State]
+STATE_UNSPECIFIED: State
 QUEUED: State
-RECONCILED: State
+OPEN: State
+APPLIED: State
 FAILED: State
 NO_CHANGES: State
 IGNORED: State
-
-class IngestionDataSource(_message.Message):
-    __slots__ = ("name", "api_key")
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    API_KEY_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    api_key: str
-    def __init__(self, name: _Optional[str] = ..., api_key: _Optional[str] = ...) -> None: ...
-
-class RetrieveIngestionDataSourcesRequest(_message.Message):
-    __slots__ = ("name", "sdk_name", "sdk_version")
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    SDK_NAME_FIELD_NUMBER: _ClassVar[int]
-    SDK_VERSION_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    sdk_name: str
-    sdk_version: str
-    def __init__(self, name: _Optional[str] = ..., sdk_name: _Optional[str] = ..., sdk_version: _Optional[str] = ...) -> None: ...
-
-class RetrieveIngestionDataSourcesResponse(_message.Message):
-    __slots__ = ("ingestion_data_sources",)
-    INGESTION_DATA_SOURCES_FIELD_NUMBER: _ClassVar[int]
-    ingestion_data_sources: _containers.RepeatedCompositeFieldContainer[IngestionDataSource]
-    def __init__(self, ingestion_data_sources: _Optional[_Iterable[_Union[IngestionDataSource, _Mapping]]] = ...) -> None: ...
+ERRORED: State
 
 class IngestionError(_message.Message):
     __slots__ = ("message", "code", "details")
@@ -88,17 +68,19 @@ class IngestionMetrics(_message.Message):
     def __init__(self, total: _Optional[int] = ..., queued: _Optional[int] = ..., reconciled: _Optional[int] = ..., failed: _Optional[int] = ..., no_changes: _Optional[int] = ...) -> None: ...
 
 class ChangeSet(_message.Message):
-    __slots__ = ("id", "data", "branch_id")
+    __slots__ = ("id", "data", "branch_id", "deviation_name")
     ID_FIELD_NUMBER: _ClassVar[int]
     DATA_FIELD_NUMBER: _ClassVar[int]
     BRANCH_ID_FIELD_NUMBER: _ClassVar[int]
+    DEVIATION_NAME_FIELD_NUMBER: _ClassVar[int]
     id: str
     data: bytes
     branch_id: str
-    def __init__(self, id: _Optional[str] = ..., data: _Optional[bytes] = ..., branch_id: _Optional[str] = ...) -> None: ...
+    deviation_name: str
+    def __init__(self, id: _Optional[str] = ..., data: _Optional[bytes] = ..., branch_id: _Optional[str] = ..., deviation_name: _Optional[str] = ...) -> None: ...
 
 class IngestionLog(_message.Message):
-    __slots__ = ("id", "data_type", "state", "request_id", "ingestion_ts", "producer_app_name", "producer_app_version", "sdk_name", "sdk_version", "entity", "error", "change_set")
+    __slots__ = ("id", "data_type", "state", "request_id", "ingestion_ts", "producer_app_name", "producer_app_version", "sdk_name", "sdk_version", "entity", "error", "change_set", "object_type")
     ID_FIELD_NUMBER: _ClassVar[int]
     DATA_TYPE_FIELD_NUMBER: _ClassVar[int]
     STATE_FIELD_NUMBER: _ClassVar[int]
@@ -111,6 +93,7 @@ class IngestionLog(_message.Message):
     ENTITY_FIELD_NUMBER: _ClassVar[int]
     ERROR_FIELD_NUMBER: _ClassVar[int]
     CHANGE_SET_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_TYPE_FIELD_NUMBER: _ClassVar[int]
     id: str
     data_type: str
     state: State
@@ -123,10 +106,11 @@ class IngestionLog(_message.Message):
     entity: _ingester_pb2.Entity
     error: IngestionError
     change_set: ChangeSet
-    def __init__(self, id: _Optional[str] = ..., data_type: _Optional[str] = ..., state: _Optional[_Union[State, str]] = ..., request_id: _Optional[str] = ..., ingestion_ts: _Optional[int] = ..., producer_app_name: _Optional[str] = ..., producer_app_version: _Optional[str] = ..., sdk_name: _Optional[str] = ..., sdk_version: _Optional[str] = ..., entity: _Optional[_Union[_ingester_pb2.Entity, _Mapping]] = ..., error: _Optional[_Union[IngestionError, _Mapping]] = ..., change_set: _Optional[_Union[ChangeSet, _Mapping]] = ...) -> None: ...
+    object_type: str
+    def __init__(self, id: _Optional[str] = ..., data_type: _Optional[str] = ..., state: _Optional[_Union[State, str]] = ..., request_id: _Optional[str] = ..., ingestion_ts: _Optional[int] = ..., producer_app_name: _Optional[str] = ..., producer_app_version: _Optional[str] = ..., sdk_name: _Optional[str] = ..., sdk_version: _Optional[str] = ..., entity: _Optional[_Union[_ingester_pb2.Entity, _Mapping]] = ..., error: _Optional[_Union[IngestionError, _Mapping]] = ..., change_set: _Optional[_Union[ChangeSet, _Mapping]] = ..., object_type: _Optional[str] = ...) -> None: ...
 
 class RetrieveIngestionLogsRequest(_message.Message):
-    __slots__ = ("page_size", "state", "data_type", "request_id", "ingestion_ts_start", "ingestion_ts_end", "page_token", "only_metrics")
+    __slots__ = ("page_size", "state", "data_type", "request_id", "ingestion_ts_start", "ingestion_ts_end", "page_token", "only_metrics", "object_type")
     PAGE_SIZE_FIELD_NUMBER: _ClassVar[int]
     STATE_FIELD_NUMBER: _ClassVar[int]
     DATA_TYPE_FIELD_NUMBER: _ClassVar[int]
@@ -135,6 +119,7 @@ class RetrieveIngestionLogsRequest(_message.Message):
     INGESTION_TS_END_FIELD_NUMBER: _ClassVar[int]
     PAGE_TOKEN_FIELD_NUMBER: _ClassVar[int]
     ONLY_METRICS_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_TYPE_FIELD_NUMBER: _ClassVar[int]
     page_size: int
     state: State
     data_type: str
@@ -143,7 +128,8 @@ class RetrieveIngestionLogsRequest(_message.Message):
     ingestion_ts_end: int
     page_token: str
     only_metrics: bool
-    def __init__(self, page_size: _Optional[int] = ..., state: _Optional[_Union[State, str]] = ..., data_type: _Optional[str] = ..., request_id: _Optional[str] = ..., ingestion_ts_start: _Optional[int] = ..., ingestion_ts_end: _Optional[int] = ..., page_token: _Optional[str] = ..., only_metrics: bool = ...) -> None: ...
+    object_type: str
+    def __init__(self, page_size: _Optional[int] = ..., state: _Optional[_Union[State, str]] = ..., data_type: _Optional[str] = ..., request_id: _Optional[str] = ..., ingestion_ts_start: _Optional[int] = ..., ingestion_ts_end: _Optional[int] = ..., page_token: _Optional[str] = ..., only_metrics: bool = ..., object_type: _Optional[str] = ...) -> None: ...
 
 class RetrieveIngestionLogsResponse(_message.Message):
     __slots__ = ("logs", "metrics", "next_page_token")
@@ -154,3 +140,93 @@ class RetrieveIngestionLogsResponse(_message.Message):
     metrics: IngestionMetrics
     next_page_token: str
     def __init__(self, logs: _Optional[_Iterable[_Union[IngestionLog, _Mapping]]] = ..., metrics: _Optional[_Union[IngestionMetrics, _Mapping]] = ..., next_page_token: _Optional[str] = ...) -> None: ...
+
+class RetrieveDeviationsRequest(_message.Message):
+    __slots__ = ("page_size", "page_token", "ingestion_ts_start", "ingestion_ts_end", "state", "object_type", "branch_id", "site")
+    PAGE_SIZE_FIELD_NUMBER: _ClassVar[int]
+    PAGE_TOKEN_FIELD_NUMBER: _ClassVar[int]
+    INGESTION_TS_START_FIELD_NUMBER: _ClassVar[int]
+    INGESTION_TS_END_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_TYPE_FIELD_NUMBER: _ClassVar[int]
+    BRANCH_ID_FIELD_NUMBER: _ClassVar[int]
+    SITE_FIELD_NUMBER: _ClassVar[int]
+    page_size: int
+    page_token: str
+    ingestion_ts_start: int
+    ingestion_ts_end: int
+    state: _containers.RepeatedScalarFieldContainer[State]
+    object_type: _containers.RepeatedScalarFieldContainer[str]
+    branch_id: _containers.RepeatedScalarFieldContainer[str]
+    site: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, page_size: _Optional[int] = ..., page_token: _Optional[str] = ..., ingestion_ts_start: _Optional[int] = ..., ingestion_ts_end: _Optional[int] = ..., state: _Optional[_Iterable[_Union[State, str]]] = ..., object_type: _Optional[_Iterable[str]] = ..., branch_id: _Optional[_Iterable[str]] = ..., site: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class DeviationError(_message.Message):
+    __slots__ = ("message", "code")
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    CODE_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    code: int
+    def __init__(self, message: _Optional[str] = ..., code: _Optional[int] = ...) -> None: ...
+
+class Change(_message.Message):
+    __slots__ = ("id", "object_type", "object_primary_value", "change_type", "before", "after")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_TYPE_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_PRIMARY_VALUE_FIELD_NUMBER: _ClassVar[int]
+    CHANGE_TYPE_FIELD_NUMBER: _ClassVar[int]
+    BEFORE_FIELD_NUMBER: _ClassVar[int]
+    AFTER_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    object_type: str
+    object_primary_value: str
+    change_type: str
+    before: bytes
+    after: bytes
+    def __init__(self, id: _Optional[str] = ..., object_type: _Optional[str] = ..., object_primary_value: _Optional[str] = ..., change_type: _Optional[str] = ..., before: _Optional[bytes] = ..., after: _Optional[bytes] = ...) -> None: ...
+
+class Deviation(_message.Message):
+    __slots__ = ("id", "ingestion_ts", "last_update_ts", "name", "source", "state", "object_type", "branch_id", "ingested_entity", "error", "changes")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    INGESTION_TS_FIELD_NUMBER: _ClassVar[int]
+    LAST_UPDATE_TS_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    OBJECT_TYPE_FIELD_NUMBER: _ClassVar[int]
+    BRANCH_ID_FIELD_NUMBER: _ClassVar[int]
+    INGESTED_ENTITY_FIELD_NUMBER: _ClassVar[int]
+    ERROR_FIELD_NUMBER: _ClassVar[int]
+    CHANGES_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    ingestion_ts: int
+    last_update_ts: int
+    name: str
+    source: str
+    state: State
+    object_type: str
+    branch_id: str
+    ingested_entity: _ingester_pb2.Entity
+    error: DeviationError
+    changes: _containers.RepeatedCompositeFieldContainer[Change]
+    def __init__(self, id: _Optional[str] = ..., ingestion_ts: _Optional[int] = ..., last_update_ts: _Optional[int] = ..., name: _Optional[str] = ..., source: _Optional[str] = ..., state: _Optional[_Union[State, str]] = ..., object_type: _Optional[str] = ..., branch_id: _Optional[str] = ..., ingested_entity: _Optional[_Union[_ingester_pb2.Entity, _Mapping]] = ..., error: _Optional[_Union[DeviationError, _Mapping]] = ..., changes: _Optional[_Iterable[_Union[Change, _Mapping]]] = ...) -> None: ...
+
+class RetrieveDeviationsResponse(_message.Message):
+    __slots__ = ("deviations", "next_page_token")
+    DEVIATIONS_FIELD_NUMBER: _ClassVar[int]
+    NEXT_PAGE_TOKEN_FIELD_NUMBER: _ClassVar[int]
+    deviations: _containers.RepeatedCompositeFieldContainer[Deviation]
+    next_page_token: str
+    def __init__(self, deviations: _Optional[_Iterable[_Union[Deviation, _Mapping]]] = ..., next_page_token: _Optional[str] = ...) -> None: ...
+
+class RetrieveDeviationByIDRequest(_message.Message):
+    __slots__ = ("id",)
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    def __init__(self, id: _Optional[str] = ...) -> None: ...
+
+class RetrieveDeviationByIDResponse(_message.Message):
+    __slots__ = ("deviation",)
+    DEVIATION_FIELD_NUMBER: _ClassVar[int]
+    deviation: Deviation
+    def __init__(self, deviation: _Optional[_Union[Deviation, _Mapping]] = ...) -> None: ...
